1fab6546adf6fbf0157662e0f2bedf2485595006
sakujyo <tomo3u3u@gmail.com>
2018-07-31 16:48:24 +0900

    add Python dict sample

---
+++ dict.py
@@ -0,0 +1,13 @@
+def test_dict():
+    d = {1: 0}
+    d[2] = 3
+    print(type(d), d)
+    for k, v in d.items():
+        print("{0}: {1}".format(k, v))
+    s = sorted(d.items(), key=lambda kv: -kv[1])
+    print(type(s), s)
+    for k, v in s:
+        print("{0}: {1}".format(k, v))
+
+if __name__ == '__main__':
+    test_dict()
