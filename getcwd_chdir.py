b6145c38ff6eb16ef6eef104ddbeb3ed47a8b6fc
sakujyo <tomo3u3u@gmail.com>
2018-08-05 02:34:27 +0900

    add time.process_time()

--- getcwd_chdir.py
+++ getcwd_chdir.py
@@ -1,12 +1,14 @@
 # https://qiita.com/Oz-Spade/items/7fa479fae7a0ea6fcddf
 import os
+import time
 
 def test_getcwd():
-    print(os.getpid())
+    print("pid: {}".format(os.getpid()))
     print(os.getcwd())
     os.chdir('C:\\tmp')
     print(os.getcwd())
     print(dir(os))
+    print(time.process_time())
 
 if __name__ == '__main__':
     test_getcwd()
